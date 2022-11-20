bdd685c92c87bbf2d85f712687d9738a147ceafc
Andrew Drozdov <andrew@mrdrozdov.com>
2022-11-20 10:31:18 -0500

    Add user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_205729) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_153020) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_205729) do
     t.datetime "updated_at", null: false
     t.string "status", default: "public"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
